363fa966e1645af488c296f5e5cd6a28a4543afb
EEEYeSeong <179356885+EEEYeSeong@users.noreply.github.com>
2024-09-08 11:10:35 +0900

    빌드 스크립트 정리

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ PROJECT_DIR=$(pwd)
 
 # Patch
 patch -d ORB_SLAM3/include -p2 --binary < patch/System.h.patch
+sed -i 's/++11/++14/g' ORB_SLAM3/CMakeLists.txt
 
+# Build
 cd ORB_SLAM3/
 chmod +x ./build.sh
-sed -i 's/++11/++14/g' CMakeLists.txt
 ./build.sh
 cd $PROJECT_DIR
 
